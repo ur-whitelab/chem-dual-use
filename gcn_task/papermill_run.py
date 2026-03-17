8a68219059f1b313dbd544fd2803622b43fb0186
qcampbel <qcampbe2@ur.rochester.edu>
2026-03-16 20:13:42 -0400

    automate postprocess and add STONED analysis

--- gcn_task/papermill_run.py
+++ gcn_task/papermill_run.py
@@ -1,5 +1,6 @@
 import papermill as pm
 import os
+import shutil
 print(os.getcwd())
 
 censor_region = 'above'
@@ -14,9 +15,8 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_split': censor_split,
                             'censor_region': censor_region,
                         },
-                        
     )
-    pm.execute_notebook(input_path='ynoise_gcn.ipynb', 
+    pm.execute_notebook(input_path='ynoise_gcn.ipynb',
                         output_path=f'{output_dir}/ynoise_gcn_split{censor_split}_{censor_region}.ipynb',
                         allow_errors=True,
                         parameters={
@@ -24,7 +24,7 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_region': censor_region,
                         },
     )
-    pm.execute_notebook(input_path='xnoise_gcn.ipynb', 
+    pm.execute_notebook(input_path='xnoise_gcn.ipynb',
                         output_path=f'{output_dir}/xnoise_gcn_split{censor_split}_{censor_region}.ipynb',
                         allow_errors=True,
                         parameters={
@@ -32,4 +32,21 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_region': censor_region,
                         },
     )
+
+postprocess_src = 'gcn_results/postprocess_stuff'
+postprocess_dst = 'OUTPUTS/postprocess_stuff'
+os.makedirs(postprocess_dst, exist_ok=True)
+for fname in sorted(os.listdir(postprocess_src)):
+    if fname.endswith('.ipynb'):
+        shutil.copy(os.path.join(postprocess_src, fname), os.path.join(postprocess_dst, fname))
+original_dir = os.getcwd()
+os.chdir(postprocess_dst)
+for fname in sorted(os.listdir('.')):
+    if fname.endswith('.ipynb'):
+        print(f'Running {fname}')
+        pm.execute_notebook(input_path=fname,
+                            output_path=fname,
+                            allow_errors=True,
+        )
+os.chdir(original_dir)
 
--- mlp_task/papermill_run.py
+++ mlp_task/papermill_run.py
@@ -1,5 +1,6 @@
 import papermill as pm
 import os
+import shutil
 print(os.getcwd())
 
 censor_region = 'above'
@@ -7,7 +8,7 @@ output_dir = 'OUTPUTS/notebooks'
 os.makedirs(output_dir, exist_ok=True)
 for censor_split in [0.1, 0.5, 0.9]:
     print(f'Running MLP notebooks for split {censor_split}')
-    pm.execute_notebook(input_path='omit_mlp.ipynb', 
+    pm.execute_notebook(input_path='omit_mlp.ipynb',
                         output_path=f'{output_dir}/omit_mlp_split{censor_split}_{censor_region}.ipynb',
                         allow_errors=True,
                         parameters={
@@ -15,7 +16,7 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_region': censor_region,
                         },
     )
-    pm.execute_notebook(input_path='xnoise_mlp.ipynb', 
+    pm.execute_notebook(input_path='xnoise_mlp.ipynb',
                         output_path=f'{output_dir}/xnoise_mlp_split{censor_split}_{censor_region}.ipynb',
                         allow_errors=True,
                         parameters={
@@ -23,7 +24,7 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_region': censor_region,
                         },
     )
-    pm.execute_notebook(input_path='ynoise_mlp.ipynb', 
+    pm.execute_notebook(input_path='ynoise_mlp.ipynb',
                         output_path=f'{output_dir}/ynoise_mlp_split{censor_split}_{censor_region}.ipynb',
                         allow_errors=True,
                         parameters={
@@ -31,3 +32,13 @@ for censor_split in [0.1, 0.5, 0.9]:
                             'censor_region': censor_region,
                         },
     )
+
+shutil.copy('mlp_results/mlp_main_plots.ipynb', 'OUTPUTS/mlp_main_plots.ipynb')
+original_dir = os.getcwd()
+os.chdir('OUTPUTS')
+print('Running mlp_main_plots.ipynb')
+pm.execute_notebook(input_path='mlp_main_plots.ipynb',
+                    output_path='mlp_main_plots.ipynb',
+                    allow_errors=True,
+)
+os.chdir(original_dir)
